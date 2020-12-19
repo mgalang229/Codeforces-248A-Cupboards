de17949d63d714a761e0dc5cca2e388603665a07
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-19 23:39:33 +0800

    Create java_sol.java

---
+++ java_sol.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static final Reader in = new Reader();
+	static final PrintWriter out = new PrintWriter(System.out);
+
+	public static void main(String[] args) {
+//		int t=in.nextInt();
+		int t=1;
+		for(int i=1; i<=t; ++i)
+			new Solver();
+		out.close();
+	}
+	
+	static class Solver {
+		Solver() {
+			int n=in.nextInt();
+			int l=0, r=0;
+			for(int i=0; i<n; ++i) {
+				int a=in.nextInt(), b=in.nextInt();
+				if(a==0)
+					l++;
+				if(b==0)
+					r++;
+			}
+			out.println(Math.min(l, n-l)+Math.min(r, n-r));
+		}
+	}
+	
+	static class Reader {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		String next() {
+			while(st==null||!st.hasMoreTokens()) {
+				try {
+					st=new StringTokenizer(in.readLine());
+				} catch(Exception e) {}
+			}	
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+	}
+	
+}
